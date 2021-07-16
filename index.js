113b27499656a80f6d510dd0e6e71b0ca5fbbbc4
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-16 00:05:23 -0500

    new employee classes with extend inheritance and working classes

--- index.js
+++ index.js
@@ -5,6 +5,49 @@ const fs = require("fs")
 //?generate a webpage that displays my teams info (emails and github profiles)
 //HTML file is generated that displays a nicely formatted team roster based on user input
 
+class Employee {
+    constructor(employeeName, id, email) {
+        this.employeeName = employeeName;
+        this.id = id;
+        this.email = email;
+    }
+}
+
+//require employee
+class Manager extends Employee {
+    //manager name, id, email, office
+    constructor(employeeName, id, email, officeNumber) {
+        super(employeeName, id, email);
+        this.officeNumber = officeNumber;
+    }
+}
+
+class Engineer extends Employee {
+    //engineer name, id, email, github
+    constructor(employeeName, id, email, githubUsername) {
+        super(employeeName, id, email);
+        this.githubUsername = githubUsername;
+    }
+}
+
+class Intern extends Employee {
+    //intern name, id, email, school
+    constructor(employeeName, id, email, school) {
+        super(employeeName, id, email);
+        this.school = school;
+    }
+}
+
+jack = new Manager("jack", 584968, "jack@work.com", 169)
+brandon = new Engineer("brandon", 156615, "brandon@work.com", "brandonnorsworthy")
+zach = new Engineer("zach", 156515, "zach@work.com", "zach1234")
+willy = new Intern("willy", 564648, "willy@work.com", "University of Texas")
+
+console.log("1", jack)
+console.log("2", brandon)
+console.log("3", zach)
+console.log("4", willy)
+
 //!js
     //?start the application
     //am prompted to enter the team manager’s name, employee ID, email address, and office number
