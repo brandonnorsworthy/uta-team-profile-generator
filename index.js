a73e7e79db1fa3a767d9b748e18b079e70a8f5f0
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-16 03:24:09 -0500

    added all questions and functions to call files that generate data based on answers

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const inquirer = require("inquirer")
-const jest = require("jest")
-const fs = require("fs")
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
 
-const Engineer = require("./lib/Engineer")
-const Intern = require("./lib/Intern")
-const Manager = require("./lib/Manager")
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const generateDynamicCards =  require('./lib/generateDynamicCards');
+const generateHtml = require('./lib/generateHtml');
 
 //?generate a webpage that displays my teams info (emails and github profiles)
 //HTML file is generated that displays a nicely formatted team roster based on user input
 
-// jack = new Manager("jack", 584968, "jack@work.com", 169)
-// brandon = new Engineer("brandon", 156615, "brandon@work.com", "brandonnorsworthy")
-// zach = new Engineer("zach", 156515, "zach@work.com", "zach1234")
-// willy = new Intern("willy", 564648, "willy@work.com", "University of Texas")
-
-// console.log("1", jack)
-// console.log("2", brandon)
-// console.log("3", zach)
-// console.log("4", willy)
-
 const employeeQuestions = [ //questions that are uniform for all basic employees
-    {type: 'input', message: "Enter the Team Manager's name", name: 'employeeName'},
-    {type: 'input', message: "Enter the Team Manager's employee ID", name: 'id'},
-    {type: 'input', message: "Enter the Team Manager's email address", name: 'email'},
+    {
+        type: 'input',
+        message: (prompt) => prompt.role ? `Enter the ${prompt.role}'s name` : `Enter the Team Managers's name`,
+        name: 'employeeName'
+    },
+    {
+        type: 'input',
+        message: (prompt) => prompt.role ? `Enter the ${prompt.role}'s employee ID` : `Enter the Team Managers's employee ID`,
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: (prompt) => prompt.role ? `Enter the ${prompt.role}'s email address` : `Enter the Team Managers's email address`,
+        name: 'email'
+    },
 ]
-
-let team = []
+let team = [] // array containing all team employee objects
 
 function init(){
     //?start the application
@@ -33,28 +36,79 @@ function init(){
     inquirer
     .prompt([
         ...employeeQuestions,
-        {type: 'input', message: "Enter the manager's office number", name: 'office'},
+        {
+            type: 'input',
+            message: `Enter the Team Managers's office number`,
+            name: 'office'},
+        //?enter the team manager’s name, employee ID, email address, and office number
+        //presented with a menu with the option to add an engineer or an intern or to finish building my team
+        {
+            type: 'confirm',
+            message: `Would you like to add another employee?`,
+            name: 'newEmployee'
+        },
         ])
     .then((value) => {
-        team.push(new Manager(...Object.values(value)))
-        console.log(team)
+        team.push(new Manager(...Object.values(value)));
+        value.newEmployee ? createEmployee() : createHtml();
     })
 }
 
+function createEmployee() {
+    inquirer
+    .prompt([
+    {
+        type: 'list',
+        message: 'Is this new employee an Engineer or Intern?',
+        name: 'role',
+        choices: ['Engineer', 'Intern']
+    },
+    ...employeeQuestions,
+    {
+        //?select the engineer option
+        //prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+        type: 'input',
+        message: (prompt) => `Enter the ${prompt.role}'s github username`,
+        when: (prompt) => prompt.role === 'Engineer',
+        name: 'githubUsername'
+    },
+    {
+        //?select the intern option
+        //prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+        type: 'input',
+        message: (prompt) => `Enter the ${prompt.role}'s school`,
+        when: (prompt) => prompt.role === 'Intern',
+        name: 'school'
+    },
+    {
+        type:'confirm',
+        message: `Would you like to add another employee?`,
+        name: 'newEmployee'
+    },
+    ])
+    .then((value) => {
+        //?decide to finish building my team
+        //exit the application, and the HTML is generated
+        value.role == 'Engineer' ? team.push(new Engineer(value.employeeName, value.id, value.email, value.githubUsername))
+        : value.role == 'Intern' ? team.push(new Intern(value.employeeName, value.id, value.email, value.school))
+        : console.log("Error", value.role);
+
+        value.newEmployee ? createEmployee() : createHtml();
+    })
+}
+
+function createHtml() {
+    let allTeamCardsStr = ``
+    for (teamMember of team) {
+        allTeamCardsStr += generateDynamicCards(teamMember);
+    }
+    writeToFile(generateHtml(allTeamCardsStr));
+}
+
+function writeToFile(htmlData) {
+    fs.writeFile("index.html", htmlData, (error) =>
+        error ? console.log("error occured while writing file") : console.log("successfully written to file")
+    );
+}
+
 init()
-
-//!js
-    //?enter the team manager’s name, employee ID, email address, and office number
-    //presented with a menu with the option to add an engineer or an intern or to finish building my team
-    //?select the engineer option
-    //prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-    //?select the intern option
-    //prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-    //?decide to finish building my team
-    //exit the application, and the HTML is generated
-
-//!html
-    //?clickable email address
-    //default email program opens and populates the TO field of the email with the address
-    //?clickable github username
-    //GitHub profile opens in a new tab
