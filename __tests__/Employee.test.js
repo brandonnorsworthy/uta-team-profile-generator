8931d0cab64daf7a6cd2c037e448726ce16c6505
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-16 22:30:15 -0500

    move tests into different directory to match requirements

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -23,8 +23,8 @@ describe("Employee", () => {
     })
 
     // test if can set name via constructor
-    describe("New Employee", () => {
-        test('should become a object when use new employee', () => {
+    describe("Employee Name", () => {
+        test('should set employee name equal to 1st param', () => {
             const test = "Bob";
             const obj = new Employee(test);
 
@@ -33,8 +33,8 @@ describe("Employee", () => {
     })
 
     // test if can set id via constructor
-    describe("New Employee", () => {
-        test('should become a object when use new employee', () => {
+    describe("Employee Id", () => {
+        test('should set employee id equal to 2nd param', () => {
             const test = "1234";
             const obj = new Employee('name', test);
 
@@ -43,8 +43,8 @@ describe("Employee", () => {
     })
 
     // test if can set email via constructor
-    describe("New Employee", () => {
-        test('should become a object when use new employee', () => {
+    describe("Employee Email", () => {
+        test('should set employee email equal to 3rd param', () => {
             const test = "person@email.com";
             const obj = new Employee('email', 'id', test);
 
@@ -53,11 +53,50 @@ describe("Employee", () => {
     })
 
     //test to see if class name returns "Employee"
-    describe("New Employee", () => {
+    describe("Employee Class Name", () => {
         test('should become a object when use new employee', () => {
             const obj = new Employee();
 
             expect(obj.constructor.name).toEqual("Employee");
+        })
+    })
+
+    //test to see if getName() returns name
+    describe("Get Employee name", () => {
+        test('should return employee name when call getName()', () => {
+            const test = "Bobby"
+            const obj = new Employee(test);
+
+            expect(obj.getName()).toEqual(test);
+        })
+    })
+
+    //test to see if getId() returns id
+    describe("Get Employee id", () => {
+        test('should return employee id when call getId()', () => {
+            const test = "123456"
+            const obj = new Employee("name", test);
+
+            expect(obj.getId()).toEqual(test);
+        })
+    })
+
+    //test to see if getEmail() returns email
+    describe("Get Employee email", () => {
+        test('should return employee email when call getEmail()', () => {
+            const test = "email@email.com"
+            const obj = new Employee("name", "id", test);
+
+            expect(obj.getEmail()).toEqual(test);
+        })
+    })
+
+    //test to see if getRole() returns role
+    describe("Get Employee role name", () => {
+        test('should return employee role when call getRole()', () => {
+            const obj = new Employee();
+
+            expect(obj.getRole()).toEqual("Employee");
         })
     })
 });
