0557e6f8577256dd921fc3ec1139b8b0da264c23
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-16 20:53:48 -0500

    Manager class passes all classes

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,9 +1,31 @@
-//require Manager 
+//require Manager
+const Manager = require("../lib/Manager");
 
-//create test on manager class
+describe("Manager", () => {
+    describe("Initialization", () => {
+        test("Manager class contains name, id, email, officeNumber", () => {
+            //create test on manager class
+            const obj = new Manager();
 
-//test to see if you can set officeNumber using our constructor
+            expect("employeeName" in obj).toEqual(true);
+            expect("id" in obj).toEqual(true);
+            expect("email" in obj).toEqual(true);
+            expect("officeNumber" in obj).toEqual(true);
+        })
 
-//test to see if getRole() returns "Manager"
+        test("Manager class contains name, id, email, officeNumber", () => {
+            //test to see if you can set officeNumber using our constructor
+            const test = "teststring"
+            const obj = new Manager("name", "id", "email", test);
 
-//test to see if getOfficeNumber() returns officeNumber test value
+            expect(obj.officeNumber).toEqual(test);
+        })
+
+        test("Manager class contains name, id, email, officeNumber", () => {
+            //test to see if class name returns "Manager"
+            const obj = new Manager();
+
+            expect(obj.constructor.name).toEqual("Manager");
+        })
+    })
+});
