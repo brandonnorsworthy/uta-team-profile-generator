c958b70457adea4a8d1a1c8ee33addda8fa01ed9
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-16 22:38:53 -0500

    updated intern tests to getrole and getschool

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,30 +2,42 @@
 const Intern = require("../lib/Intern");
 
 describe("Intern", () => {
-    describe("Initialization", () => {
-        test("Intern class contains name, id, email, school", () => {
-            //create test on Intern class
-            const obj = new Intern();
+    test("Intern class contains name, id, email, school", () => {
+        //create test on Intern class
+        const obj = new Intern();
 
-            expect("employeeName" in obj).toEqual(true);
-            expect("id" in obj).toEqual(true);
-            expect("email" in obj).toEqual(true);
-            expect("school" in obj).toEqual(true);
-        })
+        expect("employeeName" in obj).toEqual(true);
+        expect("id" in obj).toEqual(true);
+        expect("email" in obj).toEqual(true);
+        expect("school" in obj).toEqual(true);
+    })
 
-        test("Intern class contains name, id, email, school", () => {
-            //test to see if you can set school using our constructor
-            const test = "teststring"
-            const obj = new Intern("name", "id", "email", test);
+    test("Intern class contains name, id, email, school", () => {
+        //test to see if you can set school using our constructor
+        const test = "teststring"
+        const obj = new Intern("name", "id", "email", test);
 
-            expect(obj.school).toEqual(test);
-        })
+        expect(obj.school).toEqual(test);
+    })
 
-        test("Intern class contains name, id, email, school", () => {
-            //test to see if class name returns "Intern"
-            const obj = new Intern();
+    test("Intern class contains name, id, email, school", () => {
+        //test to see if class name returns "Intern"
+        const obj = new Intern();
 
-            expect(obj.constructor.name).toEqual("Intern");
-        })
+        expect(obj.constructor.name).toEqual("Intern");
+    })
+
+    test('should return intern school when call getSchool()', () => {
+        const test = "University of Testing"
+        const obj = new Intern("name", "id", "email", test);
+
+        expect(obj.getSchool()).toEqual(test);
+    })
+
+    //test to see if getRole() returns role
+    test('should return Intern role when call getRole()', () => {
+        const obj = new Intern();
+
+        expect(obj.getRole()).toEqual("Intern");
     })
 });
