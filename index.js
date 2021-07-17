720993ca2dda52b9d799860e261de4689bca7649
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-16 22:48:29 -0500

    removed generating javascript from class directory and updated imports

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-const generateDynamicCards =  require('./lib/generateDynamicCards');
-const generateHtml = require('./lib/generateHtml');
+const generateDynamicCards =  require('./src/generateDynamicCards');
+const generateHtml = require('./src/generateHtml');
 
 //?generate a webpage that displays my teams info (emails and github profiles)
 //HTML file is generated that displays a nicely formatted team roster based on user input
